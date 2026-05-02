afdbfdfc4fc087ace0a426e97c58435ef2865bcf
Gabriela de Paula Silvestre Correa <gabriela.dpaula.scorrea@gmail.com>
2026-05-02 20:19:28 -0300

    Add files via upload

---
+++ lista de nomes.js
@@ -0,0 +1,10 @@
+// Lista com nomes
+let nomes = ["Gabi", "Grazi", "Mariana", "Pedro", "Yasmin"];
+
+// Percorre os índices do array
+for (let indice in nomes) {
+
+    // Mostra a posição e o nome correspondente
+    console.log(`Posição ${indice}: ${nomes[indice]}`);
+
+}
